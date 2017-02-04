0c4362d07c4523e88126755b9c86ed70958c3cc9
nhoughto5 <nhoughto@uvic.ca>
2017-02-04 10:44:03 -0800

    S3E77

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -2,14 +2,15 @@
 
 #include "BuildingEscape.h"
 #include "Grabber.h"
-
+#define OUT
 
 // Sets default values for this component's properties
-UGrabber::UGrabber()
+UGrabber::UGrabber() : reach(100.f)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	//bWantsBeginPlay = true;
 
 	// ...
 }
@@ -27,10 +28,37 @@ void UGrabber::BeginPlay()
 
 
 // Called every frame
-void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	/// Get Player viewpoint
+	FVector playerViewPointLocation;
+	FRotator playerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT playerViewPointLocation, OUT playerViewPointRotation);
+
+	/// Draw red trace
+	FVector LineTraceEnd = playerViewPointLocation + playerViewPointRotation.Vector() * reach;
+	DrawDebugLine(GetWorld(), playerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 0.f, 10.0f);
+
+	/// Setup Query Parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+	/// Line-Trace (ray-cast) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		playerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+	/// Find what we hit.
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit) {
+		UE_LOG(LogTemp, Warning, TEXT("Contact with: %s"), *(ActorHit->GetName()));
+	}
+
+	
+	
 }
 
